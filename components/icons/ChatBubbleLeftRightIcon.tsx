8f99923eb6ceaf86a0aac7a545ba3ae809c09a09
rgrams-coder <rgrams.co@gmail.com>
2025-08-10 10:38:25 +0530

    updated admin dashboard

---
+++ components/icons/ChatBubbleLeftRightIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ChatBubbleLeftRightIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193l-3.72 3.72a1.125 1.125 0 01-1.59 0l-3.72-3.72a1.125 1.125 0 01-1.59 0l-3.72 3.72a1.125 1.125 0 01-1.59 0l-3.72-3.72a1.125 1.125 0 01-1.59 0L.322 8.511c.884-.284 1.5-1.128 1.5-2.097V4.286c0-1.136.847-2.1 1.98-2.193l3.72-3.72a1.125 1.125 0 011.59 0l3.72 3.72a1.125 1.125 0 011.59 0l3.72-3.72a1.125 1.125 0 011.59 0l3.72 3.72a1.125 1.125 0 011.59 0l3.72-3.72c.884.284 1.5 1.128 1.5 2.097v4.286z"
+    />
+  </svg>
+);
+
+export default ChatBubbleLeftRightIcon;
