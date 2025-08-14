1045f6b4ca2c26b17d6a85682efd26b953536b72
rgrams-coder <rgrams.co@gmail.com>
2025-08-15 05:09:14 +0530

    added some disclaimer

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -114,7 +114,7 @@ const LandingPage = forwardRef<LandingPageHandles, LandingPageProps>(({ onGetSta
                 <div className="max-w-4xl mx-auto text-center">
                     <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
                         <p className="text-yellow-800 font-medium">
-                            This site is still in development phase. Kindly support us by registration.
+                            Thank You for visiting the site. The site is under maintainance .
                         </p>
                     </div>
                 </div>
